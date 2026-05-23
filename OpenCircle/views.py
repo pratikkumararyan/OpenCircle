fa21871475ca01f2960fbd1e1d56082f40b5f8c0
pratikkumararyan <pratikumararyan@gmail.com>
2026-05-23 22:33:57 +0530

    Added form functionality

--- OpenCircle/views.py
+++ OpenCircle/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     if request.method == "POST":
-        print(request.POST)
+        
+        return HttpResponse(round((float(request.POST['rating'])/25)+1, 1))
     else:
         return render(request, 'landing.html')
 
 def account(request):
     return HttpResponse('This is the account creation page')
 
+# 4 -> 75, 
+# 5 -> 100, 
+# 1 -> 0, 
+# 3 -> 50
