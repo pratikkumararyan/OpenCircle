85cb26e2500bdaac8cae227c0b4f0a680695d525
pratikkumararyan <pratikumararyan@gmail.com>
2026-05-26 16:07:28 +0530

    messing around with HTMX

--- OpenCircle/views.py
+++ OpenCircle/views.py
@@ -2,16 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    if request.method == "POST":
+    if request.META.get("HTTP_HTMX_REQUEST"):
+        return render(request, "Message Submitted! ")
         
-        return HttpResponse(round((float(request.POST['rating'])/25)+1, 1))
+        #HttpResponse(round((float(request.POST['rating'])/25)+1, 1))
     else:
         return render(request, 'landing.html')
 
 def account(request):
     return HttpResponse('This is the account creation page')
-
-# 4 -> 75, 
-# 5 -> 100, 
-# 1 -> 0, 
-# 3 -> 50
