5dd1e9fdcad685579019f414d9ae3f70a567be28
pratikkumararyan <pratikumararyan@gmail.com>
2026-07-01 06:41:54 +0530

    Idk how how to do HTMX atp tbh

--- OpenCircle/views.py
+++ OpenCircle/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     if request.META.get("HTTP_HTMX_REQUEST"):
-        return render(request, "Message Submitted! ")
-        
+        print("smth")
+        #return render(request, "Message Submitted! ")
+        return HttpResponse('<input type="checkbox" id="my_modal_7" class="modal-toggle" />')
         #HttpResponse(round((float(request.POST['rating'])/25)+1, 1))
     else:
         return render(request, 'landing.html')
