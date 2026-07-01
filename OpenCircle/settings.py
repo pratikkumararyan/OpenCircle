61077893f8579b25acde2eeac5773c9958d3287c
pratikkumararyan <pratikumararyan@gmail.com>
2026-07-01 07:01:57 +0530

    Configured Settings.py

--- OpenCircle/settings.py
+++ OpenCircle/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',
+    'template_partials'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware'
 ]
 
 ROOT_URLCONF = 'OpenCircle.urls'
--- OpenCircle/views.py
+++ OpenCircle/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import django_htmx
 
 def index(request):
-    if request.META.get("HTTP_HTMX_REQUEST"):
+    if request.htmx:
         print("smth")
         #return render(request, "Message Submitted! ")
         return HttpResponse('<input type="checkbox" id="my_modal_7" class="modal-toggle" />')
         #HttpResponse(round((float(request.POST['rating'])/25)+1, 1))
-    else:
-        return render(request, 'landing.html')
+    
+    return render(request, 'landing.html')
 
 def account(request):
     return HttpResponse('This is the account creation page')
