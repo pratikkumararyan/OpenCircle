c8a8a1100de9d197c191a2d0f96c3b7d3be5b40f
pratikkumararyan <pratikumararyan@gmail.com>
2026-07-04 19:15:44 +0530

    :Found out that the CSRF token was visible in inspect view cuz of body tag, so fixed that

--- OpenCircle/views.py
+++ OpenCircle/views.py
@@ -4,10 +4,11 @@ from .models import Suggestions
 
 def index(request):
     if request.htmx and request.POST:
+        print("htmx+POST request received")
         message = request.POST.get('message')
         rating = int(round((float(request.POST.get('rating'))/25)+1, 1))
-        entry = Suggestions.objects.create(rating=rating, message=message)
-        print(Suggestions.objects.values_list("message", flat=True))
+        # entry = Suggestions.objects.create(rating=rating, message=message)
+        # print(Suggestions.objects.values_list("message", flat=True))
         return render(request, "landingPage/partial.html")
     
     return render(request, 'landingPage/landing.html')
