fcb7e7f8ba1a4b56a5918e6d45e509f0ad67d17d
pratikkumararyan <pratikumararyan@gmail.com>
2026-08-15 13:19:00 +0530

    added a @login_required decorator to all my UI functions after importing the same from django.contrib.auth

--- ui/views.py
+++ ui/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def ui(request):
     return HttpResponse('UI Site with recommendations etc.')
 
+@login_required
 def post(request):
     return HttpResponse('Post site here')
 
+@login_required
 def dm(request):
     return HttpResponse('DM site here')
 
+@login_required
 def friends(request):
     return HttpResponse('Friends list here')
