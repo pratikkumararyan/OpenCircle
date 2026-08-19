fe39a00c260c79d5f604554b6cd4ca0a91b23f63
pratikkumararyan <pratikumararyan@gmail.com>
2026-08-19 14:18:09 +0530

    i was struggling to grasp the concept of ManyToMany fields and after much wrestling with docs and guides I finally found out the solution, which was simnply adding another field to my Profile extension and set the ref to User

--- ui/models.py
+++ ui/models.py
@@ -13,7 +13,6 @@ class Profile(models.Model):
     bio = models.CharField(max_length=15, blank=True, default="No bio added.")
     profile_picture = models.ImageField(upload_to='profile_pics', null=True, blank=True, default="profile_pics/default_user.png")
     strikes = models.IntegerField(default=0)
-    followers = models.IntegerField(default=0)
+    followers = models.ManyToManyField(User, related_name="followers")
     def __str__(self):
         return self.user.username + "'s profile"
-
