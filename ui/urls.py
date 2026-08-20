cd0e41c414d0086e70f1c3acbf1799a320254677
pratikkumararyan <pratikumararyan@gmail.com>
2026-08-20 14:12:50 +0530

    finally the media thingy worked :D

--- ui/urls.py
+++ ui/urls.py
@@ -1,11 +1,9 @@
 from . import views
 from django.urls import path
-from django.conf.urls import static
-from django.conf import settings
 
 urlpatterns = [
     path('', views.ui, name='ui'),
     path('post/', views.post, name='post'),
     path('dm/', views.dm, name='dm'),
     path('friends/', views.friends, name='friends'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- ui/views.py
+++ ui/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Profile
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
-import os
 # Create your views here.
 
 @login_required
@@ -11,9 +9,7 @@ def ui(request):
     user = request.user
     name = user.username
     profile = Profile.objects.get(user=user)
-    img = profile.profile_picture
-    
-    print(img)
+    img = profile.profile_picture.url
     return render(request, "UI/dashboard.html", {"username": name, "img": img})
 
 @login_required
