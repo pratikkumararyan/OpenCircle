283734f516d3118e27bee18dc3188d86c2f60ee3
pratikkumararyan <pratikkumararyan@gmail.com>
2026-08-25 17:48:52 +0530

    took my shot at trying to display char count yet failed

--- ui/views.py
+++ ui/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 # Create your views here.
@@ -30,3 +30,9 @@ def dm(request):
 @login_required
 def friends(request):
     return HttpResponse('Friends list here')
+
+def postMessage(request):
+    if request.htmx and request.POST:
+        message = request.POST.get("htmlField")
+        Post.objects.create(request.user, message)
+    return redirect("/")
